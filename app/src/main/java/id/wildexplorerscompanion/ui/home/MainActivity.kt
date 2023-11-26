577dea7f4a0ef3c7623f7061f50ea327943e07bd
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2023-11-26 23:39:10 +0900

    update change file location

--- app/src/main/java/id/wildexplorerscompanion/ui/home/MainActivity.kt
+++ app/src/main/java/id/wildexplorerscompanion/ui/home/MainActivity.kt
@@ -1,7 +1,8 @@
-package id.wildexplorerscompanion
+package id.wildexplorerscompanion.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.wildexplorerscompanion.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

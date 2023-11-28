fa08904e50fa0c6fe21608b4803ef3b82936cf47
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2023-11-28 18:05:59 +0900

    update adding Activity Feature

---
+++ app/src/main/java/id/wildexplorerscompanion/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,30 @@
+package id.wildexplorerscompanion.ui.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import id.wildexplorerscompanion.R
+import id.wildexplorerscompanion.databinding.ActivitySplashScreenBinding
+import id.wildexplorerscompanion.ui.home.MainActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+
+            finish()
+        }, SPLASH_DELAY)
+
+
+    }
+
+    companion object {
+        private const val SPLASH_DELAY = 3000L
+    }
+}

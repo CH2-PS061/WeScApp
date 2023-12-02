c2d82933bc5f4c957c89016c8a4481d0121657b7
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2023-12-02 11:30:35 +0900

    Update build.gradle.kts
    Change gson convertert and add logging interceptor library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,8 @@ dependencies {
 
     //Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 
     //MarkdownView
     implementation ("com.github.mukeshsolanki:MarkdownView-Android:2.0.0")

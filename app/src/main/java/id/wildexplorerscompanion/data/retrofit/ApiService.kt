57b34858f4e13fb3572436d7edee7e86e3551f01
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2023-12-02 19:16:54 +0900

    update API Service

--- app/src/main/java/id/wildexplorerscompanion/data/retrofit/ApiService.kt
+++ app/src/main/java/id/wildexplorerscompanion/data/retrofit/ApiService.kt
@@ -1,4 +1,24 @@
 package id.wildexplorerscompanion.data.retrofit
 
+import id.wildexplorerscompanion.data.retrofit.response.LoginResponse
+import id.wildexplorerscompanion.data.retrofit.response.RegisterResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface ApiService {
+    @FormUrlEncoded
+    @POST("register")
+    suspend fun register(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): RegisterResponse
+
+    @FormUrlEncoded
+    @POST("login")
+    suspend fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): LoginResponse
 }

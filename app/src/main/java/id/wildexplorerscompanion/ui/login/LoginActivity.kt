7e720fd91524bd165dda654195c72c268aae8e52
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2023-12-02 19:17:18 +0900

    update adding Logic

--- app/src/main/java/id/wildexplorerscompanion/ui/login/LoginActivity.kt
+++ app/src/main/java/id/wildexplorerscompanion/ui/login/LoginActivity.kt
@@ -10,17 +10,33 @@ import android.text.TextPaint
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
 import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
 import id.wildexplorerscompanion.R
 import id.wildexplorerscompanion.databinding.ActivityLoginBinding
+import id.wildexplorerscompanion.ui.ViewModelFactory
 import id.wildexplorerscompanion.ui.home.MainActivity
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private val loginViewModel by viewModels<LoginViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
         singUpText()
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.edtTextEmail.text.toString()
+            val password = binding.edtTextPassword.text.toString()
+            loginViewModel.getLogin(email, password)
+        }
+
+        loginViewModel.loginResponse.observe(this){
+
+        }
     }
 
 
@@ -45,5 +61,9 @@ class LoginActivity : AppCompatActivity() {
         binding.tvSingup.text = spanString
         binding.tvSingup.highlightColor = Color.TRANSPARENT
         binding.tvSingup.movementMethod = LinkMovementMethod.getInstance()
+    }
+
+    private fun showToast(message: String){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
     }
 }

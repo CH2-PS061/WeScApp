702bacc771605a579c2b0ce8e006cc20871a1548
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2023-12-02 19:14:30 +0900

    update add ViewModelFactory

---
+++ app/src/main/java/id/wildexplorerscompanion/ui/ViewModelFactory.kt
@@ -0,0 +1,27 @@
+package id.wildexplorerscompanion.ui
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import id.wildexplorerscompanion.data.repo.WildRepository
+import id.wildexplorerscompanion.di.Injection
+import id.wildexplorerscompanion.ui.login.LoginViewModel
+
+class ViewModelFactory(private val repository: WildRepository): ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)){
+            return LoginViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknow ViewModel Class: ${modelClass.name}")
+    }
+
+    companion object {
+        @Volatile
+        private var instance: ViewModelFactory?  =null
+        fun getInstance(context: Context): ViewModelFactory =
+            instance ?: synchronized(this){
+                instance ?: ViewModelFactory(Injection.provideRepository(context))
+            }.also { instance = it}
+    }
+}

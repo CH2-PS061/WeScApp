0a1eb510718d4b555140640a2ebab8263807ab98
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2023-12-03 13:35:30 +0900

    update for DataStore

--- app/src/main/java/id/wildexplorerscompanion/data/repo/WildRepository.kt
+++ app/src/main/java/id/wildexplorerscompanion/data/repo/WildRepository.kt
@@ -1,10 +1,13 @@
 package id.wildexplorerscompanion.data.repo
 
+import id.wildexplorerscompanion.data.pref.UserModel
+import id.wildexplorerscompanion.data.pref.UserPreference
 import id.wildexplorerscompanion.data.retrofit.ApiService
 import id.wildexplorerscompanion.data.retrofit.response.LoginResponse
 import id.wildexplorerscompanion.data.retrofit.response.RegisterResponse
+import kotlinx.coroutines.flow.Flow
 
-class WildRepository private constructor(private val apiService: ApiService ){
+class WildRepository private constructor(private val apiService: ApiService,private val userPreference: UserPreference){
 
     suspend fun register(name: String, email: String, password: String): RegisterResponse {
         return apiService.register(name, email, password)
@@ -15,16 +18,27 @@ class WildRepository private constructor(private val apiService: ApiService ){
     }
 
 
+    //User Preferences
+    fun getSession(): Flow<UserModel>{
+        return userPreference.getSession()
+    }
 
+    suspend fun saveSession(user: UserModel){
+        userPreference.saveSession(user)
+    }
+
+    suspend fun logout() = userPreference.logout()
 
     companion object {
         @Volatile
         private var instance: WildRepository? = null
         fun getInstance(
-            apiService: ApiService
+            apiService: ApiService,
+            userPreference: UserPreference
         ): WildRepository =
             instance ?: synchronized(this){
-                instance ?: WildRepository(apiService)
+                instance ?: WildRepository(apiService,userPreference)
             }.also { instance = it }
     }
+
 }

bb9a29cdf87ec342d2b7e43f0c7bfad34ae3ab8f
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2023-12-08 09:32:50 +0900

    update fixing code

--- app/src/main/java/id/wildexplorerscompanion/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/id/wildexplorerscompanion/ui/splashscreen/SplashScreenActivity.kt
@@ -4,19 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import id.wildexplorerscompanion.databinding.ActivitySplashScreenBinding
 import id.wildexplorerscompanion.ui.login.LoginActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashScreenBinding
+    private val handler: Handler = Handler(Looper.getMainLooper())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        Handler().postDelayed({
-            startActivity(Intent(this,LoginActivity::class.java))
 
+        handler.postDelayed({
+            startActivity(Intent(this,LoginActivity::class.java))
             finish()
         }, SPLASH_DELAY)
     }

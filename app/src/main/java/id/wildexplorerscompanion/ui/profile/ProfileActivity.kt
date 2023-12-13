9c2a25704960fd2e38f281aa0778a54e618d27ac
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2023-12-13 18:24:08 +0900

    update add profile UI

--- app/src/main/java/id/wildexplorerscompanion/ui/profile/ProfileActivity.kt
+++ app/src/main/java/id/wildexplorerscompanion/ui/profile/ProfileActivity.kt
@@ -1,12 +1,20 @@
 package id.wildexplorerscompanion.ui.profile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import id.wildexplorerscompanion.R
 import id.wildexplorerscompanion.databinding.ActivityProfileBinding
+import id.wildexplorerscompanion.ui.ViewModelFactory
+import id.wildexplorerscompanion.ui.login.LoginActivity
+import id.wildexplorerscompanion.ui.resetpassword.ResetPasswordActivity
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var profileBinding: ActivityProfileBinding
+    private val profileViewModel by viewModels<ProfileViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         profileBinding = ActivityProfileBinding.inflate(layoutInflater)
@@ -16,6 +24,21 @@ class ProfileActivity : AppCompatActivity() {
         supportActionBar?.title = getString(R.string.profile)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        profileBinding.btnChangePassword.setOnClickListener {
+            startActivity(Intent(this@ProfileActivity, ResetPasswordActivity::class.java))
+        }
 
+        profileBinding.btnLogout.setOnClickListener {
+            profileViewModel.logout()
+            startActivity(Intent(this@ProfileActivity, LoginActivity::class.java))
+            finish()
+        }
+
+        profileViewModel.getSession().observe(this){
+            profileBinding.nameUser.text = it.name
+        }
+
+        // TODO: Buat Delete Akun dan Change Image Profile belum bisa 
     }
+
 }

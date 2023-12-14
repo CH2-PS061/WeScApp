4fe059047a19e6f8e61262ef467b77f1e0ed254a
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2023-12-14 20:03:51 +0900

    fix some logic and add dialog

--- app/src/main/java/id/wildexplorerscompanion/ui/register/RegisterActivity.kt
+++ app/src/main/java/id/wildexplorerscompanion/ui/register/RegisterActivity.kt
@@ -9,8 +9,13 @@ import android.text.Spanned
 import android.text.TextPaint
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import id.wildexplorerscompanion.R
 import id.wildexplorerscompanion.databinding.ActivityRegisterBinding
 import id.wildexplorerscompanion.ui.ViewModelFactory
@@ -26,6 +31,7 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
         signInText()
+        showCostumeDialog()
 
         binding.btnRegister.setOnClickListener {
             val name = binding.edtPersonName.text.toString()
@@ -33,6 +39,8 @@ class RegisterActivity : AppCompatActivity() {
             val password = binding.edtTextPassword.text.toString()
             registerViewModel.getRegister(name,email,password)
         }
+
+
     }
     private fun signInText(){
         val text ="Sudah Punya Akun? Masuk Disini"
@@ -55,5 +63,29 @@ class RegisterActivity : AppCompatActivity() {
         binding.tvRegister.text = spanString
         binding.tvRegister.highlightColor = Color.TRANSPARENT
         binding.tvRegister.movementMethod = LinkMovementMethod.getInstance()
+    }
+
+    private fun showCostumeDialog(){
+        val builder = AlertDialog.Builder(this)
+        val costumeAlertDialog = LayoutInflater.from(this).inflate(R.layout.dialog_success,null)
+        registerViewModel.registerResponse.observe(this){
+            if (!it.success){
+                showToast(it.message)
+            }else{
+                builder.setView(costumeAlertDialog)
+                val btnClose = costumeAlertDialog.findViewById<Button>(R.id.btn_dismiss)
+                btnClose.setOnClickListener {
+                    val intentLogin = Intent(this, LoginActivity::class.java)
+                    startActivity(intentLogin)
+                    finish()
+                }
+                val dialog = builder.create()
+                dialog.show()
+            }
+        }
+    }
+
+    private fun showToast(message: String){
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show()
     }
 }

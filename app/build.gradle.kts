8dc2ff4d87cacb6ffb416cb017b19ebf1667706e
Dewantara <46391302+GoodGuyzaxx@users.noreply.github.com>
2023-12-19 21:40:28 +0900

    remove compose lib and feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,14 +42,10 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.2"
-    }
     buildFeatures{
         viewBinding = true
         buildConfig = true
         mlModelBinding = true
-        compose = true
     }
 }
 
@@ -70,14 +66,6 @@ dependencies {
 
     //MarkdownView
     implementation ("com.github.mukeshsolanki:MarkdownView-Android:2.0.0")
-
-    //Compose
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.activity:activity-compose:1.8.1")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
 
     //Lifecycle
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
